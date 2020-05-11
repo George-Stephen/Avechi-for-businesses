af8d7f61fbdaf00db80b80e8016bea793c349d5c
George-Stephen <yazz777stevie@gmail.com>
2020-05-11 11:59:59 +0300

    adding the classes and database

---
+++ src/main/resources/db/create.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE azure ;
+\c azure
+CREATE TABLE business(id serial PRIMARY KEY , name VARCHAR, owner VARCHAR, email VARCHAR , phone VARCHAR , category VARCHAR , website VARCHAR , creation TIMESTAMP );
+CREATE TABLE  signin( id serial PRIMARY KEY , name VARCHAR ,email VARCHAR, phone VARCHAR, password VARCHAR );
+CREATE TABLE review( id serial PRIMARY KEY , businessid INT, review VARCHAR );
+CREATE DATABASE azure_test WITH TEMPLATE azure ;
