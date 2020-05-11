37442d413d2abad0f0ce82be21061982fc200b46
George-Stephen <yazz777stevie@gmail.com>
2020-05-11 12:37:48 +0300

    added the categories table

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -3,4 +3,5 @@ CREATE DATABASE azure ;
 CREATE TABLE business(id serial PRIMARY KEY , name VARCHAR, owner VARCHAR, email VARCHAR , phone VARCHAR , category VARCHAR , website VARCHAR , creation TIMESTAMP );
 CREATE TABLE  signin( id serial PRIMARY KEY , name VARCHAR ,email VARCHAR, phone VARCHAR, password VARCHAR );
 CREATE TABLE review( id serial PRIMARY KEY , businessid INT, review VARCHAR );
+CREATE TABLE categories(id serial PRIMARY KEY, name VARCHAR);
 CREATE DATABASE azure_test WITH TEMPLATE azure ;
