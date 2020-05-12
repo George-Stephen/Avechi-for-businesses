1347ad86579f994386aa4e39361b0ee57403256f
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-05-12 14:30:47 +0300

    Created the add method

--- src/main/java/dao/Sql2oCategoryDao.java
+++ src/main/java/dao/Sql2oCategoryDao.java
@@ -1,12 +1,30 @@
 package dao;
 
 import objects.category;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
 import java.util.List;
 
 public class Sql2oCategoryDao implements CategoryDao {
+
+    private final Sql2o sql2o;
+
+    public Sql2oCategoryDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
     @Override
     public void add(category newCategory) {
+        try(Connection con=sql2o.open()){
+            String sql="INSERT INTO category (name,description) VALUES (:name,:description) ";
+
+            int id=(int) con.createQuery(sql,true)
+                    .bind(newCategory)
+                    .executeUpdate()
+                    .getKey();
+            newCategory.setId(id);
+        }
 
     }
 
