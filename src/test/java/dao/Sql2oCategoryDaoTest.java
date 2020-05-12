fe12019ab9840b44c732b70d2258a9be08a48f51
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-05-12 14:39:26 +0300

    Created a test class for Category

---
+++ src/test/java/dao/Sql2oCategoryDaoTest.java
@@ -0,0 +1,28 @@
+package dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Sql2oCategoryDaoTest {
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void updateCategoryById() {
+    }
+
+    @Test
+    public void deleteCategoryById() {
+    }
+
+    @Test
+    public void clearAll() {
+    }
+}
