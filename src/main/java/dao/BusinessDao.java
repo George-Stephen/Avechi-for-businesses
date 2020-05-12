d10a6266108cfb5ec74d125935aba7528f2e1f3a
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-05-12 10:59:52 +0300

    Created CRUD methods in the BusinessDao interface and added a business_users table in create.sql

--- src/main/java/dao/BusinessDao.java
+++ src/main/java/dao/BusinessDao.java
@@ -1,4 +1,28 @@
 package dao;
 
+import objects.Business;
+import objects.User;
+
+import java.util.List;
+
 public interface BusinessDao {
+
+    //create
+
+    void add(Business business, User user);
+
+    //read
+
+    List<Business> getAll();
+    List<Business> searchByCategory(String category);
+    Business findById(int id);
+    List<Business> filterByMostRecent();
+
+
+    //update
+    void updateBusinessById(int id);
+
+    //delete
+
+    void deleteById(int business_id,int user_id);
 }
--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -4,4 +4,5 @@ CREATE TABLE business(id serial PRIMARY KEY , name VARCHAR, owner VARCHAR, email
 CREATE TABLE  signin( id serial PRIMARY KEY , name VARCHAR ,email VARCHAR, phone VARCHAR, password VARCHAR );
 CREATE TABLE review( id serial PRIMARY KEY , businessid INT, review VARCHAR );
 CREATE TABLE categories(id serial PRIMARY KEY, name VARCHAR, description VARCHAR);
+CREATE TABLE business_users(id serial PRIMARY KEY, user_id INT,business_id INT);
 CREATE DATABASE azure_test WITH TEMPLATE azure ;
