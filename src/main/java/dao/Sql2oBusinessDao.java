bf6c8b1e9e95aa904ce78415184355bb13d907cf
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-05-12 11:01:26 +0300

    Implemented BusinessDao interface to Sql2oBusinessDao class and overided the methods in BusinessDao

--- src/main/java/dao/Sql2oBusinessDao.java
+++ src/main/java/dao/Sql2oBusinessDao.java
@@ -1,4 +1,43 @@
 package dao;
 
-public class Sql2oBusinessDao {
+import objects.Business;
+import objects.User;
+
+import java.util.List;
+
+public class Sql2oBusinessDao implements BusinessDao {
+    @Override
+    public void add(Business business, User user) {
+
+    }
+
+    @Override
+    public List<Business> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Business> searchByCategory(String category) {
+        return null;
+    }
+
+    @Override
+    public Business findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Business> filterByMostRecent() {
+        return null;
+    }
+
+    @Override
+    public void updateBusinessById(int id) {
+
+    }
+
+    @Override
+    public void deleteById(int business_id, int user_id) {
+
+    }
 }
