d2f65c848b2887549721b170ea15843a228d6583
Nyawira Muturi <albinamuturi@gmail.com>
2020-05-13 10:30:52 +0300

    run tests for setters

--- src/test/java/objects/LoginTest.java
+++ src/test/java/objects/LoginTest.java
@@ -12,12 +12,26 @@ public class LoginTest{
         Login testLogin = new Login("Nyawira", "12345");
         assertEquals(true, testLogin instanceof Login);
     }
+    @Test
     public void getName(){
         Login testLogin = new Login("Nyawira", "12345");
         assertEquals("Nyawira", testLogin.getName());
     }
+    @Test
+    public void setName(){
+        Login testLogin = new Login("Nyawira", "12345");
+        testLogin.setName("Nyawira");
+        assertEquals("Nyawira", testLogin.getName());
+    }
+    @Test
     public void getPassword(){
         Login testLogin = new Login("Nyawira", "12345");
+        assertEquals("12345", testLogin.getPassword());
+    }
+    @Test
+    public void setPassword(){
+        Login testLogin = new Login("Nyawira", "12345");
+        testLogin.setPassword("12345");
         assertEquals("12345", testLogin.getPassword());
     }
 }
