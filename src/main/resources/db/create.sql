8fe842b45a18500c726ced307c60a47201217c68
Nyawira Muturi <albinamuturi@gmail.com>
2020-05-13 13:57:58 +0300

    add Sql2o logic for login

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -4,6 +4,7 @@ CREATE TABLE business(id serial PRIMARY KEY , name VARCHAR, owner VARCHAR, email
 INT
 , website VARCHAR , creation TIMESTAMP );
 CREATE TABLE  signin( id serial PRIMARY KEY , name VARCHAR ,email VARCHAR, phone VARCHAR, password VARCHAR );
+CREATE TABLE login(id serial PRIMARY KEY, name VARCHAR, email VARCHAR);
 CREATE TABLE review( id serial PRIMARY KEY , businessid INT, review VARCHAR, writtenBy VARCHAR, createdat TIMESTAMP );
 CREATE TABLE categories(id serial PRIMARY KEY, name VARCHAR, description VARCHAR);
 CREATE TABLE signin_review(id serial PRIMARY KEY, signin_id INTEGER, review_id INTEGER);
